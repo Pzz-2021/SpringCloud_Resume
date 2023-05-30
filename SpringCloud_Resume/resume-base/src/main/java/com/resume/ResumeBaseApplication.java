342e63921780dcfbf11a1b51db82a57cc087cf4f
PP-2022 <2733286080@qq.com>
2023-05-30 20:41:35 +0800

    创建初始工程、父工程和base工程

---
+++ SpringCloud_Resume/resume-base/src/main/java/com/resume/ResumeBaseApplication.java
@@ -0,0 +1,13 @@
+package com.resume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeBaseApplication.class, args);
+    }
+
+}
