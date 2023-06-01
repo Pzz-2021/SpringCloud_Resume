12aa7cb6d920d63ee64be076d7e141385ef6bae2
年少有为 <1470511515@qq.com>
2023-06-01 21:58:24 +0800

    统一返回类

---
+++ resume-base/src/main/java/com/resume/base/model/RestResponse.java
@@ -0,0 +1,57 @@
+package com.resume.base.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/*
+ *@filename: RestResponse
+ *@author: lyh
+ *@date:2023/6/1 21:49
+ *@version 1.0
+ *@description TODO
+ */
+@Data
+public class RestResponse<T> implements Serializable {
+    private Integer code; //编码：1成功，0和其它数字为失败
+    private String message; //错误信息
+    private T data; //数据
+    //把构造方法私有
+    private RestResponse() {
+    }
+    public static <T> RestResponse<T> success() {
+        RestResponse<T> response = new RestResponse<T>();
+        response.code = 1;
+        return response;
+    }
+    public static <T> RestResponse<T> success(T object) {
+        RestResponse<T> response = success();
+        response.code = 1;
+        response.data = object;
+        return response;
+    }
+    public static <T> RestResponse<T> error() {
+        RestResponse<T> response = new RestResponse<T>();
+        response.code = 0;
+        return response;
+    }
+    public static <T> RestResponse<T> error(T object) {
+        RestResponse<T> response = new RestResponse<T>();
+        response.code = 0;
+        response.data=object;
+        return response;
+    }
+    public static <T> RestResponse<T> error(String message) {
+        RestResponse<T> response = new RestResponse<T>();
+        response.code = 0;
+        response.message=message;
+        return response;
+    }
+    public static <T> RestResponse<T> error(String message,T object) {
+        RestResponse<T> response = new RestResponse<T>();
+        response.code = 0;
+        response.message=message;
+        response.data=object;
+        return response;
+    }
+}
