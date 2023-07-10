4d9b4a15de06dd6a764a79f16c27a12426bbbfc1
PP-2022 <2733286080@qq.com>
2023-07-10 16:15:56 +0800

    添加移动云OCR接口，修改简历解析模块，判断文件后缀是否为图片、word、pdf

--- resume-parse/src/main/java/com/resume/parse/service/ResumeService.java
+++ resume-parse/src/main/java/com/resume/parse/service/ResumeService.java
@@ -6,17 +6,26 @@ import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.resume.base.utils.DateUtil;
-import com.resume.parse.dto.PhasedOutDTO;
+import com.resume.dubbo.api.PositionService;
+import com.resume.dubbo.domian.ResumeStateDTO;
 import com.resume.parse.mapper.ResumeMapper;
 import com.resume.parse.pojo.Resume;
-import com.resume.parse.utils.RedisConstants;
+import com.resume.parse.utils.OCRUtil;
+import com.resume.parse.utils.URLUtil;
+import com.resume.parse.utils.UploadUtil;
 import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 
 /**
  * <p>
@@ -42,43 +51,70 @@ public class ResumeService extends ServiceImpl<ResumeMapper, Resume> {
 
     private static final String PHASEDOUT = "已淘汰";
 
+    private static final String PDF_PATH = "D:/code/pythonProject1/uie_v1/data/temp/";
+
     public void parseResume(Long pkResumeId, String url) {
         Resume resume = new Resume();
         resume.setPkResumeId(pkResumeId);
 
+        String text = "";
+        String pdfUrl = "";
+
         // 获取简历文字信息
-        String text = restTemplate.getForObject(PATH + "/get-word-string?url=" + url, String.class);
-//        System.out.println(text);
+        String fileExtension = URLUtil.getFileExtension(url);
+        switch (fileExtension) {
+            case ".jpeg":
+            case ".jpg":
+            case ".png":
+                text = OCRUtil.parseImage(url);
+                break;
+            case ".pdf":
+                text = restTemplate.getForObject(PATH + "/get-word-string?url=" + url, String.class);
+                pdfUrl = url;
+                break;
+            case ".word":
+                text = restTemplate.getForObject(PATH + "/get-word-string?url=" + url, String.class);
+
+                // 上传pdf文件
+                try {
+                    Path path = Paths.get(PDF_PATH + URLUtil.getFileBaseName(url) + ".pdf");
+                    byte[] fileBytes = Files.readAllBytes(path);
+                    pdfUrl = uploadUtil.uploadByBytes(fileBytes, UUID.randomUUID().toString() + '-' + URLUtil.getFileBaseName(url) + ".pdf");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
         resume.setResumeContent(text);
+        resume.setPdfUrl(pdfUrl);
+
 
         // 将文字信息解析，得到 JSON 的String信息
-        text = restTemplate.getForObject(PATH + "/parseString?txt=" + text, String.class);
-//        System.out.println("\n\n" + text);
+        String jsonString = restTemplate.getForObject(PATH + "/parseString?txt=" + text, String.class);
 
         // Unicode 转义的 JSON 数据 转义回来
-        String json = StringEscapeUtils.unescapeJava(text);
+        String json = StringEscapeUtils.unescapeJava(jsonString);
 
         // 解析为JSON数据
         JSONObject jsonObject = JSONObject.parseObject(json);
-//        System.out.println(jsonObject);
 
         Set<Map.Entry<String, Object>> entries = jsonObject.entrySet();
 
         for (Map.Entry<String, Object> entry : entries) {
-            // 得到基础信息
+            // 得到基础信息，保存数据库
             if (entry.getKey().equals("resume")) {
                 JSONArray value = (JSONArray) entry.getValue();
                 resume = BeanUtil.fillBeanWithMap((Map<String, Object>) value.get(0), resume, false);
-//                System.out.println("resume = " + resume);
-
+            // System.out.println("resume = " + resume);
             } else {
                 // 其他信息
                 System.out.println(entry.getValue());
             }
         }
 
+        // 把基础信息移除
         jsonObject.remove("resume");
-//        System.out.println(jsonObject);
+
         resume.setJsonContent(String.valueOf(jsonObject));
 
         // 设置resume状态已解析
@@ -106,5 +142,4 @@ public class ResumeService extends ServiceImpl<ResumeMapper, Resume> {
     public boolean phasedOutResume(ResumeStateDTO resumeStateDTO) {
         return changeResumeState(resumeStateDTO);
     }
-}
 }
